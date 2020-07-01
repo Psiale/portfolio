4a7613fc69ad73c77e31c0dba6615ecfea31d03d
psialedev <psialedev@gmail.com>
2020-07-01 15:26:53 -0500

    adding js script

---
+++ main.js
@@ -0,0 +1,7 @@
+let welcomeContainer = document.getElementById("welcome-container")
+let infoContainer = document.getElementById("info-container")
+
+function welcomeHidder() {
+    welcomeContainer.style.display = "none";
+    infoContainer.style.display = "initial";
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  purge: [],
   theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    extend: {
+      gridTemplateRows: {
+        // Simple 8 row grid
+       '8': 'repeat(8, minmax(0, 1fr))',
+        
+        // Complex site-specific row configuration
+       'layout': '200px minmax(900px, 1fr) 100px',
+      }
+    }
+  }
 }

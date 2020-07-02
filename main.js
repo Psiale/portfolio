ec04a74e1902bf48d0140fba2dafc9f2b7bf8873
psialedev <psialedev@gmail.com>
2020-07-02 16:44:13 -0500

    finally created the grid as i wanted

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let welcomeContainer = document.getElementById("welcome-container")
 let infoContainer = document.getElementById("info-container")
+let hello = document.getElementById("hello")
 
 function welcomeHidder() {
     welcomeContainer.style.display = "none";
-    infoContainer.style.display = "initial";
+    // infoContainer.style.display = "grid";
+    hello.style.display = "sticky"
 }

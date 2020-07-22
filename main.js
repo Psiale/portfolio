ae0b2c2ceb2c835372a5c24e6a47e821d903c3ac
Alexis <psialedev@gmail.com>
2020-07-22 13:14:46 -0500

    added meta tag

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let welcomeContainer = document.getElementById("welcome-container")
-let infoContainer = document.getElementById("info-container")
-let hello = document.getElementById("hello")
-let welcome = document.getElementById("welcome")
+let welcomeContainer = document.getElementById("welcome-container");
+let infoContainer = document.getElementById("info-container");
+let hello = document.getElementById("hello");
+let welcome = document.getElementById("welcome");
 
 function welcomeHidder() {
-    welcomeContainer.style.display = "none";
-    infoContainer.style.display = "grid";
-    hello.style.display = "fixed"
+  welcomeContainer.style.display = "none";
+  infoContainer.style.display = "grid";
+  hello.style.display = "fixed";
 }
